97d8e0e384f94428d0d68bd91d7da554c70acda2
frovai <34864119+frovai@users.noreply.github.com>
2019-05-23 15:41:43 -0300

    Update deploy-api.sh

--- deploy-api.sh
+++ deploy-api.sh
@@ -5,5 +5,5 @@ sudo cp poc-teste/terraform-files/spring.yml .
 sudo sed "s/1/$TAG/g" spring.yml > spring-final.yml
 sudo /usr/bin/docker-compose -f /home/ec2-user/spring-final.yml down
 sudo /usr/bin/docker-compose -f /home/ec2-user/spring-final.yml build --no-cache
-sudo sleep 10
+sudo sleep 30
 sudo /usr/bin/docker-compose -f /home/ec2-user/spring-final.yml up -d
