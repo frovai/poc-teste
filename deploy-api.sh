8d19181518d0b902d1d8cbe411a268fc736b9a71
frovai <34864119+frovai@users.noreply.github.com>
2019-05-23 15:07:07 -0300

    Create deploy-api.sh

---
+++ deploy-api.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TAG=$1
+
+sudo cp poc-teste/terraform-files/spring.yml .
+sudo sed "s/1/$TAG/g" spring.yml > spring-final.yml
+sudo /usr/bin/docker-compose -f /home/ec2-user/spring-final.yml down
+sudo /usr/bin/docker-compose -f /home/ec2-user/spring-final.yml build --no-cache
+sudo /usr/bin/docker-compose -f /home/ec2-user/spring-final.yml up -d
